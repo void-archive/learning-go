b7e8d8c2e668d950b8d9ef1098100de8f3b457e7
Ryan <RyanMcKeown82@Gmail.com>
2018-07-04 15:22:51 +0800

    put main.go in a math/ dir and edit file contents
    also made a mistake on the first iteration Println

--- src/math/main.go
+++ src/math/main.go
@@ -3,10 +3,11 @@ package main
 import "fmt"
 
 func main() {
+  // := is a shorthand for var a <type> = <value>
   x := 5
   y := 10
   sum := x + y
   
-  fmt.Println('Sum of x plus y:')
+  fmt.Println("Sum of x plus y:")
   fmt.Println(sum)
 }
