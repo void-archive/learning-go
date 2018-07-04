ecc925a028ef189571e2226945ec5fe3ca59a4c7
Ryan <RyanMcKeown82@Gmail.com>
2018-07-04 15:35:40 +0800

    fix comment

--- src/math/main.go
+++ src/math/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-  // := is a shorthand for var a <type> = <value>
+  // `:=` is a shorthand for `var <name> <type> = <value>`
   x := 5
   y := 10
   sum := x + y
